a268b3c6e23a3cebc67777242fd5c3855ca9415b
Neuro Developer <neurodeveloper@isd.org.br>
2020-05-24 09:10:26 -0300

    Update downloadfromMS.py

--- downloadfromMS.py
+++ downloadfromMS.py
@@ -10,9 +10,7 @@ driver.get('https://covid.saude.gov.br/')
 python_button = driver.find_elements_by_tag_name("ION-BUTTON")
 python_button[3].ATTR = 'TXT:Arquivo<SP>CSV'
 fi = python_button[3].click()
-time.sleep(10)
-fi = python_button[3].click()
-time.sleep(10)
+time.sleep(20)
 driver.quit()
 
 os.system('cd /home/travis/Downloads \n ls')
