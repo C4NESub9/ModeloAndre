35b7a022c5ffaa45d775831751800426eb7ef74d
Neuro Developer <neurodeveloper@isd.org.br>
2020-05-24 02:08:10 -0300

    selenium

--- downloadfromMS.py
+++ downloadfromMS.py
@@ -17,16 +17,10 @@ SELENIUM_WEBDRIVERS = {
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
-#options.binary_location = "D:\Projetos\ModeloAndre"
 driver = webdriver.Chrome(options=options)
 driver.get('https://covid.saude.gov.br/')
 
-# click radio button
 python_button = driver.find_elements_by_tag_name("ION-BUTTON")
 python_button[3].ATTR = 'TXT:Arquivo<SP>CSV'
-#python_button = driver.find_element_by_class_name('button-white')
 fi = python_button[3].click()
 time.sleep(60)
-print(' ')
-
-#TYPE=ION-BUTTON ATTR=TXT:Arquivo<SP>CSV
