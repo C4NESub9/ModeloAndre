d2041701d1dfc5b1cc426728f83035b126071040
Neuro Developer <neurodeveloper@isd.org.br>
2020-05-26 16:52:58 -0300

    Consertando erro de predição

--- main.cpp
+++ main.cpp
@@ -181,10 +181,10 @@ void calculaModeloARXMQ(std::string matrixIn, std::string matrixOut, double Isol
 
     for(unsigned i = 0; i < 7; ++i){
         temp = arx->sim(Isolamento,temp);
-        predictOutput(0,i) = (int)temp;
+        predictOutput(0,i+atrasoEnvolvido) = (int)temp;
     }
     data = ((~(Output(0,from(0)-->counter-2)))|(~(estOutput(0,from(1)-->counter-1)|predictOutput))|(~(Output(0,from(0)-->counter-2)-estOutput(0,from(1)-->counter-1))));
-    //std::cout << data << std::endl;
+    std::cout << data << std::endl;
     std::cout << arx->print() << std::endl;
 }
 
