ce62c7523bc6ddf53ced4cae6c3942bf4fb17fa5
Neuro Developer <neurodeveloper@isd.org.br>
2020-05-26 15:14:10 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -176,7 +176,7 @@ void calculaModeloARXMQ(std::string matrixIn, std::string matrixOut){
     LinAlg::Matrix<double> predictOutput(1,7);
     for(unsigned i = 0; i < 7; ++i){
         temp = arx->sim(0,temp);
-        predictOutput(0,i) = (int)temp;
+        predictOutput(50,i) = (int)temp;
     }
     data = ((~(Output(0,from(0)-->counter-2)))|(~(estOutput(0,from(1)-->counter-1)|predictOutput))|(~(Output(0,from(0)-->counter-2)-estOutput(0,from(1)-->counter-1))));
     //std::cout << data << std::endl;
