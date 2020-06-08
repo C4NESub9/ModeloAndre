c048d33f09dd40716000bebfc2cdf5f18ab1953c
Neuro Developer <neurodeveloper@isd.org.br>
2020-06-08 08:43:12 -0300

    Só 2ª ordem

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ LinAlg::Matrix<double> findBestARModelMQ(LinAlg::Matrix<double> Input, LinAlg::M
     double AIC4 = (Output*(~Output))(0,0);
     double BIC = AIC4, C = AIC4;
 
-    for(uint8_t k = 2; k < 2; ++k){
+    for(uint8_t k = 2; k < 3; ++k){
         arx = new ModelHandler::ARX<double>(0,k);
         LS = new OptimizationHandler::LeastSquare<double>(arx);
         LS->Optimize(Input,Output);
@@ -58,7 +58,7 @@ LinAlg::Matrix<double> findBestARModelMQE(LinAlg::Matrix<double> Input, LinAlg::
     double AIC4 = (Output*(~Output))(0,0);
     double BIC = AIC4, C = AIC4;
 
-    for(uint8_t k = 2; k < 2; ++k){
+    for(uint8_t k = 2; k < 3; ++k){
         arx = new ModelHandler::ARX<double>(0,k);
         ELS = new OptimizationHandler::ExtendedLeastSquare<double>(arx);
         ELS->Optimize(Input,Output);
@@ -95,7 +95,7 @@ LinAlg::Matrix<double> findBestARXModelMQ(LinAlg::Matrix<double> Input, LinAlg::
     double AIC4 = (Output*(~Output))(0,0);
     double BIC = AIC4, C = AIC4;
 
-    for(uint8_t k = 1; k < 3; ++k){
+    for(uint8_t k = 2; k < 3; ++k){
         arx = new ModelHandler::ARX<double>(k,k);
         LS = new OptimizationHandler::LeastSquare<double>(arx);
         LS->Optimize(Input,Output);
@@ -132,7 +132,7 @@ LinAlg::Matrix<double> findBestARXModelMQE(LinAlg::Matrix<double> Input, LinAlg:
     double AIC4 = (Output*(~Output))(0,0);
     double BIC = AIC4, C = AIC4;
 
-    for(uint8_t k = 2; k < 2; ++k){
+    for(uint8_t k = 2; k < 3; ++k){
         arx = new ModelHandler::ARX<double>(k,k);
         ELS = new OptimizationHandler::ExtendedLeastSquare<double>(arx);
         ELS->Optimize(Input,Output);
